beba7b95557fed165339052e3cad9b0775ba59fa
sshuvoo <safayanshuvo@gmail.com>
2025-02-05 17:28:58 +0600

    [Solved 122] Best Time to Buy and Sell Stock II

--- src/122.best-time-to-buy-and-sell-stock-ii.ts
+++ src/122.best-time-to-buy-and-sell-stock-ii.ts
@@ -6,7 +6,14 @@
 
 // @lc code=start
 function maxProfit(prices: number[]): number {
-    
-};
+  if (prices.length <= 1) return 0
+  let maxProfit = 0
+  let buy = prices[0]
+  for (let i = 1; i < prices.length; i++) {
+    maxProfit += Math.max(prices[i] - buy, 0)
+    buy = prices[i]
+  }
+  return maxProfit
+}
+// https://github.com/sshuvoo
 // @lc code=end
-
