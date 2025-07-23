a033729c0b5fdbd85a3a7a1a3922640d677ecde8
Saffaullah Shuvo <safayanshuvo@gmail.com>
2025-07-24 02:35:18 +0600

    [46] Permutations

--- src/46.permutations.ts
+++ src/46.permutations.ts
@@ -5,8 +5,26 @@
  */
 
 // @lc code=start
+function swap(nums: number[], i: number, j: number): void {
+  const temp = nums[i]
+  nums[i] = nums[j]
+  nums[j] = temp
+}
 function permute(nums: number[]): number[][] {
-    
-};
+  const len = nums.length
+  const ps: number[][] = []
+  function recursiveP(i: number = 0) {
+    if (i == len) {
+      ps.push([...nums])
+    }
+    for (let j = i; j < len; j++) {
+      swap(nums, i, j)
+      recursiveP(i + 1)
+      swap(nums, i, j)
+    }
+  }
+  recursiveP()
+  return ps
+}
+// [1,2,3]
 // @lc code=end
-
--- src/90.subsets-ii.ts
+++ src/90.subsets-ii.ts
@@ -6,7 +6,21 @@
 
 // @lc code=start
 function subsetsWithDup(nums: number[]): number[][] {
-    
-};
+  const len = nums.length
+  nums.sort((a, b) => a - b)
+  const ss: number[][] = []
+  const cc: number[] = []
+  function recursiveSS(i: number = 0): void {
+    ss.push([...cc])
+    for (let j = i; j < len; j++) {
+      if (j > i && nums[j] == nums[j - 1]) continue
+      const cv = nums[j]
+      cc.push(cv)
+      recursiveSS(j + 1)
+      cc.pop()
+    }
+  }
+  recursiveSS()
+  return ss
+}
 // @lc code=end
-
