729e8360f0d76de483120270997faf60e3ba5570
Saffaullah Shuvo <safayanshuvo@gmail.com>
2025-08-04 14:22:41 +0600

    [15] 3Sum

--- src/15.3-sum.ts
+++ src/15.3-sum.ts
@@ -5,24 +5,36 @@
  */
 
 // @lc code=start
+// https://leetcode.com/u/sshuvoo/
 function threeSum(nums: number[]): number[][] {
-  nums.sort()
-  const ans: number[][] = []
+  nums.sort((a, b) => a - b)
+  const triplets: number[][] = []
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i - 1]) continue
-    let j = i + 1, k = nums.length - 1
-    while (j < k) {
-      let x = nums[i], y = nums[j], z = nums[k]
-      const sum = x + y + z
-      if (sum === 0) {
-        ans.push([x, y, z])
-        break
+    if (i > 0 && nums[i - 1] == nums[i]) continue
+    const target = -nums[i]
+    let lp = i + 1, rp = nums.length - 1
+    while (lp < rp) {
+      // avoid duplicate
+      if (lp > i + 1 && nums[lp - 1] == nums[lp]) {
+        lp++
+        continue
       }
-      else if (sum < 0) j++
-      else k--
+      // avoid duplicate
+      if (rp < nums.length - 1 && nums[rp] == nums[rp + 1]) {
+        rp--
+        continue
+      }
+      const sum = nums[lp] + nums[rp]
+      if (sum == target) {
+        triplets.push([-target, nums[lp], nums[rp]])
+        lp++
+        rp--
+      } else if (sum > target) rp--
+      else lp++
     }
   }
-  return ans
+  return triplets
 }
-// [-1,0,1,2,-1,-4]
+// console.log(threeSum([-1, 0, 1, 2, -1, -4]))
+// https://leetcode.com/u/sshuvoo/
 // @lc code=end
--- src/167.two-sum-ii-input-array-is-sorted.ts
+++ src/167.two-sum-ii-input-array-is-sorted.ts
@@ -5,13 +5,16 @@
  */
 
 // @lc code=start
+// https://github.com/sshuvoo
 function twoSum(numbers: number[], target: number): number[] {
   let lp = 0, rp = numbers.length - 1
   while (lp < rp) {
-    if (numbers[lp] + numbers[rp] == target) break
-    if (numbers[lp] + numbers[rp] > target) rp--
+    const sum = numbers[lp] + numbers[rp]
+    if (sum == target) break
+    if (sum > target) rp--
     else lp++
   }
   return [lp + 1, rp + 1]
 }
+// https://github.com/sshuvoo
 // @lc code=end
