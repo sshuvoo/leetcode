7598cf42dd801ac9ba29ec80356176e538b7b8fb
Saffaullah Shuvo <safayanshuvo@gmail.com>
2025-08-26 22:06:48 +0600

    [438] Find All Anagrams in a String

--- src/438.find-all-anagrams-in-a-string.ts
+++ src/438.find-all-anagrams-in-a-string.ts
@@ -5,27 +5,39 @@
  */
 
 // @lc code=start
+function isEqual(arr1: number[], arr2: number[]): boolean {
+  return arr1.every((val, i) => val == arr2[i])
+}
+
 function findAnagrams(s: string, p: string): number[] {
-  const sl = s.length, pl = p.length
+  const sl = s.length,
+    pl = p.length
   const ans: number[] = []
-  const fm = new Map<string, number>() // frequency map of char
-  let maxFreq = 0
-  for (let i = 0; i < sl; i++) {
-    // remove which are out of window
+  const pf: number[] = new Array<number>(26).fill(0) // frequesncy of s
+  const wf: number[] = new Array<number>(26).fill(0) // frequency of window
+  for (let i = 0; i < pl; i++) {
+    // 'a' = 97
+    pf[p.charCodeAt(i) - 97]++
+  }
+  
+  for (let i = 0; i < pl - 1; i++) {
+    wf[s.charCodeAt(i) - 97]++
+  }
+
+  for (let i = pl - 1; i < sl; i++) {
+    // decrese count if element is out of window
     if (i >= pl) {
-      const freq = fm.get(s[i - pl])!
-      if (freq > 1) {
-        fm.set(s[i - pl], freq - 1)
-      } else fm.delete(s[i - pl])
-      maxFreq--
+      wf[s.charCodeAt(i - pl) - 97]--
     }
 
-    // add new character to window
-    if(fm.has(s[i])) {
-      fm.set(s[i], fm.get(s[i])!+1)
-    } else 
-    const freq = fm.get(s[i])!
+    // add new item  to window
+    wf[s.charCodeAt(i) - 97]++
+
+    if (isEqual(pf, wf)) {
+      ans.push(i + 1 - pl)
+    }
   }
+
+  return ans
 }
-// Input: s = "cbaebabacd", p = "abc"
 // @lc code=end
