9d31f1968d43043b6397f824d48b7f968a9191af
Saffaullah Shuvo <safayanshuvo@gmail.com>
2025-09-01 01:19:13 +0600

    [448] Find All Numbers Disappeared in an Array

--- src/448.find-all-numbers-disappeared-in-an-array.ts
+++ src/448.find-all-numbers-disappeared-in-an-array.ts
@@ -6,7 +6,18 @@
 
 // @lc code=start
 function findDisappearedNumbers(nums: number[]): number[] {
-   
-};
+  const ans: number[] = []
+  for (const num of nums) {
+    const index = Math.abs(num) - 1
+    if (nums[index] > 0) {
+      nums[index] = -nums[index]
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) ans.push(i + 1)
+  }
+  return ans
+}
+// Input: nums = [-4,-3,-2,-7,8,2,-3,-1]
+// Output: [4+1,5+1]
 // @lc code=end
-
