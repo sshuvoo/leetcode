238c18bb69e4be1eef1033b4beeeadb7ec10cf00
Saffaullah Shuvo <safayanshuvo@gmail.com>
2025-09-01 17:24:04 +0600

    [54] Spiral Matrix

--- src/54.spiral-matrix.ts
+++ src/54.spiral-matrix.ts
@@ -5,20 +5,51 @@
  */
 
 // @lc code=start
+// https://github.com/sshuvoo
 function spiralOrder(matrix: number[][]): number[] {
-  const result: number[] = []
-  let sr = 0, er = matrix.length - 1, sc = 0, ec = matrix[0].length - 1
-  while (sr <= er && sc <= ec) {
-    for (let i = sc; i <= ec; i++) result.push(matrix[sr][i])
-    for (let i = sr + 1; i <= er; i++) result.push(matrix[i][ec])
-    if (sr !== er) {
-      for (let i = ec - 1; sc <= i; i--) result.push(matrix[er][i])
+  let l = 0, r = matrix[0].length - 1
+  let t = 0, b = matrix.length - 1
+  const ans: number[] = []
+
+  while (l <= r && t <= b) {
+    // top row
+    for (let j = l; j <= r; j++) {
+      ans.push(matrix[t][j])
     }
-    if (sc !== ec) {
-      for (let i = er - 1; sr < i; i--) result.push(matrix[i][sc])
+    t++
+
+    // right col
+    for (let i = t; i <= b; i++) {
+      ans.push(matrix[i][r])
     }
-    sr++; er--; sc++; ec--
+    r--
+
+    // bottom row
+    if (t <= b) {
+      for (let j = r; j >= l; j--) {
+        ans.push(matrix[b][j])
+      }
+      b--
+    }
+
+    // left col
+    if (l <= r) {
+      for (let i = b; i >= t; i--) [ans.push(matrix[i][l])]
+      l++
+    }
   }
-  return result
+
+  return ans
 }
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [1,2,3,6,9,8,7,4,5]
+// ;[
+//   [1, 2, 3, 4],
+//   [5, 6, 7, 8],
+//   [9, 10, 11, 12],
+// ]
+// l = 1, r = 2, t = 2, b = 1
+// 1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7,
+
 // @lc code=end
