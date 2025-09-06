c5164f2d5c3e1953cafe7db433665db546a1cdd1
Saffaullah Shuvo <safayanshuvo@gmail.com>
2025-09-06 22:03:31 +0600

    [222] Count Complete Tree Nodes

---
+++ src/222.count-complete-tree-nodes.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=222 lang=typescript
+ *
+ * [222] Count Complete Tree Nodes
+ */
+
+import { TreeNode } from './types.js'
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countNodes(root: TreeNode | null): number {
+  function dfs(root: TreeNode | null): number {
+    if (root == null) return 0
+    const left = dfs(root.left)
+    const right = dfs(root.right)
+    return left + right + 1
+  }
+  return dfs(root)
+}
+// @lc code=end
